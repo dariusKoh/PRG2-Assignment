fcfbec0b54b9806d68d1874177889f6cc7d2d644
dariusKoh <55723854+dariusKoh@users.noreply.github.com>
2024-01-22 09:18:38 +0800

    Delete Clive_Program.cs

--- Clive_Program.cs
+++
@@ -1,9 +0,0 @@
-/*
-//==========================================================
-// Student Number : S10257799G
-// Student Name : Giam Jun Xian Clive
-// Partner Name : Darius Koh Kai Keat
-//==========================================================
-*/
-
-using PRG2_Assignment.classes;
