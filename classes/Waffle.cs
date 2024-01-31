24d0fdce0ad7743439cf8dcd49cd0e3f82c1cbb5
CliveGiam <s10257799@connect.np.edu.sg>
2024-01-31 21:26:21 +0800

    clive comments added

--- classes/Waffle.cs
+++ classes/Waffle.cs
@@ -24,7 +24,7 @@ namespace S10257799G_PRG2Assignment
         {
             WaffleFlavour = _waffleflavour;
         }
-
+        //Function to read options.csv and get ice cream pricing based on scoops and waffle flavour 
         public void getPrices()
         {
             using (StreamReader sr = new StreamReader("data/options.csv"))
@@ -41,16 +41,18 @@ namespace S10257799G_PRG2Assignment
         public override double CalculatePrice()
         {
             getPrices();
+            //Calculate how much is owed for toppings
             double price = (1 * Toppings.Count());
-            //Match the price of scoops to the quantity
             foreach (string[] array in Prices)
             {
+                //Check if the current array options match the options of the order and set the price accordingly
                 if (Option == array[0] && Scoops == Convert.ToInt32(array[1]) && WaffleFlavour == array[3])
                 {
                     price += Convert.ToDouble(array[4]);
                     break;
                 }
             }
+            //Add 2 dollars if the flavour is premium
             foreach (Flavour item in Flavours)
             {
                 if (item.Premium == true)
